7fa05d7dac26b31af1f7445999f74cf89df63eea
NckHmml <NckHmml@users.noreply.github.com>
2015-10-06 15:04:23 +0900

    Added csharp code

---
+++ src/Solver.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuChallengeMedium.src
+{
+    public static class Solver
+    {
+        public static byte[,] Solve(byte[,] sudoku)
+        {
+            //BEGIN_CHALLENGE
+            bool[, ,] possibilities = GeneratePossibilities(sudoku);
+            SolveRecursive(sudoku, possibilities);
+            return sudoku;
+            //END_CHALLENGE
+        }
+
+        /// <summary>
+        /// Checks if a certain value in allowed within a row
+        /// </summary>
+        /// <param name="sudoku">Current sudoku field</param>
+        /// <param name="x">X position to check</param>
+        /// <param name="y">Y position to check</param>
+        /// <param name="val">Value to check</param>
+        /// <returns>If <paramref name="val"/> is posible</returns>
+        private static bool CheckRow(byte[,] sudoku, int x, int y, byte val)
+        {
+            for (int ix = 0; ix < 9; ix++)
+            {
+                if (x == ix) continue;
+                if (sudoku[ix, y] == val) return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Checks if a certain value in allowed within a column
+        /// </summary>
+        /// <param name="sudoku">Current sudoku field</param>
+        /// <param name="x">X position to check</param>
+        /// <param name="y">Y position to check</param>
+        /// <param name="val">Value to check</param>
+        /// <returns>If <paramref name="val"/> is posible</returns>
+        private static bool SolveCell(byte[,] sudoku, int x, int y, byte val)
+        {
+            for (int iy = 0; iy < 9; iy++)
+            {
+                if (y == iy) continue;
+                if (sudoku[x, iy] == val) return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Checks if a certain value in allowed within a cell
+        /// </summary>
+        /// <param name="sudoku">Current sudoku field</param>
+        /// <param name="x">X position to check</param>
+        /// <param name="y">Y position to check</param>
+        /// <param name="val">Value to check</param>
+        /// <returns>If <paramref name="val"/> is posible</returns>
+        private static bool CheckCell(byte[,] sudoku, int x, int y, byte val)
+        {
+            int startx = x - (x % 3);
+            int starty = y - (y % 3);
+
+            for (int ix = startx; ix < startx + 3; ix++)
+            {
+                for (int iy = starty; iy < starty + 3; iy++)
+                {
+                    if (y == iy && x == ix) continue;
+                    if (sudoku[ix, iy] == val) return false;
+                }
+            }
+            return true;
+        }
+
+        //BEGIN_CHALLENGE
+        public static bool SolveRecursive(byte[,] sudoku, bool[, ,] possibilities, int x = 0, int y = 0)
+        {
+            while (sudoku[x, y] != 0)
+            {
+                if (++y >= 9)
+                {
+                    y = 0;
+                    if (++x >= 9)
+                        return true;
+                }
+            }
+            int ty, tx;
+
+            for (byte val = 1; val <= 9; val++)
+            {
+                if (possibilities[x, y, val - 1])
+                {
+                    bool[, ,] clone = possibilities.Clone() as bool[, ,];
+                    SetBlock(sudoku, possibilities, x, y, val);
+                    ty = y;
+                    tx = x;
+                    if (++ty >= 9)
+                    {
+                        ty = 0;
+                        if (++tx >= 9)
+                            return true;
+                    }
+
+                    if (SolveRecursive(sudoku, possibilities, tx, ty))
+                        return true;
+                    else
+                    {
+                        possibilities = clone;
+                        sudoku[x, y] = 0;
+                    }
+                }
+            }
+
+            if (--y < 0)
+            {
+                y = 8;
+                x--;
+            }
+            return false;
+        }
+
+        public static void SetBlock(byte[,] sudoku, bool[, ,] possibilities, int x, int y, byte val)
+        {
+            sudoku[x, y] = val;
+
+            // Rows
+            for (int ix = 0; ix < 9; ix++)
+                possibilities[ix, y, val - 1] = false;
+
+            // Columns
+            for (int iy = 0; iy < 9; iy++)
+                possibilities[x, iy, val - 1] = false;
+
+            // Cell
+            int startx = x - (x % 3);
+            int starty = y - (y % 3);
+            for (int ix = startx; ix < startx + 3; ix++)
+            {
+                for (int iy = starty; iy < starty + 3; iy++)
+                    possibilities[ix, iy, val - 1] = false;
+            }
+        }
+
+        private static bool[, ,] GeneratePossibilities(byte[,] sudoku)
+        {
+            bool[, ,] ret = new bool[9, 9, 9];
+
+            for (int x = 0; x < 9; x++)
+                for (int y = 0; y < 9; y++)
+                    for (byte val = 1; val <= 9; val++)
+                    {
+                        bool possible = true;
+                        possible &= sudoku[x, y] == 0;
+                        possible &= CheckRow(sudoku, x, y, val);
+                        possible &= SolveCell(sudoku, x, y, val);
+                        possible &= CheckCell(sudoku, x, y, val);
+                        ret[x, y, val - 1] = possible;
+                    }
+
+            return ret;
+        }
+        //END_CHALLENGE
+    }
+}
---
+++ src/Tests.cs
@@ -0,0 +1,112 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuChallengeMedium.src
+{
+    [TestFixture]
+    public class Tests
+    {
+        private string Sudokus = "000000000009805100051907420290401065000000000140508093026709580005103600000000000;003020600900305001001806400008102900700000008006708200002609500800203009005010300;904200007010000000000706500000800090020904060040002000001607000000000030300005702";
+        
+        [Test]
+        public void SolveSudokus()
+        {
+            Assert.AreEqual(3, Sudokus
+                .Split(';')
+                .Select(x => ParseSudoku(x))
+                .Select(x => WriteSudoku(x))
+                .Select(x => Solver.Solve(x))
+                .Select(x => WriteSudoku(x))
+                .Count(x => IsValid(x)),
+                "One or more solutions seem to be invalid");
+        }
+
+        private byte[,] WriteSudoku(byte[,] sudoku)
+        {
+            for (int x = 0; x < 9; x++)
+            {
+                for (int y = 0; y < 9; y++)
+                {
+                    Console.Write(sudoku[x, y]);
+                }
+                Console.Write('\n');
+            }
+            Console.Write('\n');
+            return sudoku;
+        }
+
+        private byte[,] ParseSudoku(string sudoku)
+        {
+            var ret = new byte[9, 9];
+            for (int x = 0; x < 9; x++)
+                for (int y = 0; y < 9; y++)
+                    ret[y, x] = (byte)(sudoku[x + (y * 9)] - 0x30);
+
+            return ret;
+        }
+
+        private bool IsValid(byte[,] sudoku)
+        {
+            // Check the cells
+            for (int val = 1; val <= 9; val++)
+            {
+                for (int cell = 0; cell < 9; cell++)
+                {
+                    int count = 0;
+                    int startx = cell % 3;
+                    int starty = cell - (cell % 3);
+                    startx *= 3;
+
+                    for (int x = 0; x < 3; x++)
+                        for (int y = 0; y < 3; y++)
+                            if (sudoku[x + startx, y + starty] == val)
+                                if (count++ > 0)
+                                    Assert.Fail("A cell contained the same number more than once");
+                }
+            }
+
+            // Check the rows
+            for (int val = 1; val <= 9; val++)
+            {
+                for (int y = 0; y < 9; y++)
+                {
+                    int count = 0;
+                    for (int x = 0; x < 9; x++)
+                    {
+                        if (sudoku[x, y] == val)
+                            if (count++ > 0)
+                                Assert.Fail("A row contained the same number more than once");
+                    }
+                }
+            }
+
+            // Check the columns
+            for (int val = 1; val <= 9; val++)
+            {
+                for (int x = 0; x < 9; x++)
+                {
+                    int count = 0;
+                    for (int y = 0; y < 9; y++)
+                    {
+                        if (sudoku[x, y] == val)
+                            if (count++ > 0)
+                                Assert.Fail("A column contained the same number more than once");
+                    }
+                }
+            }
+
+
+            // Check the total sum
+            const int sudokusum = 9 * ((9 * (9 + 1)) / 2);
+
+            if (sudokusum != sudoku.Cast<byte>().Sum(x => x))
+                Assert.Fail("The total sum of the sudoku appears to be incorrect [{0}]", sudoku.Cast<byte>().Sum(x => x));
+
+            return true;
+        }
+    }
+}
