f84d438f20b3d51d5bb83d01877c315c3019387f
NckHmml <NckHmml@users.noreply.github.com>
2015-10-29 14:42:52 +0900

    Added GoLang code

---
+++ sudoku.go
@@ -0,0 +1,152 @@
+﻿package sudoku
+
+func Solve(data [][]int) [][]int {
+//BEGIN_CHALLENGE
+	possibilities := GeneratePossibilities(data)
+	SolveRecursive(data, possibilities, 0, 0)
+//END_CHALLENGE
+	return data
+}
+
+//BEGIN_CHALLENGE
+func SolveRecursive(data [][]int, possibilities [][][]bool, x int, y int) bool {
+	for data[x][y] != 0 {
+		y++
+		if y >= 9 {
+			y = 0 
+			x++
+			if x >= 9 {
+				return true
+			}
+		}
+	}
+
+	for val := 1; val <= 9; val++ {
+		if possibilities[x][y][val - 1] {
+			clone := CreateClone(possibilities)
+			SetBlock(data, clone, x, y, val)
+
+			tx := x
+			ty := y + 1
+			if ty >= 9 {
+				ty = 0 
+				tx++
+				if tx >= 9 {
+					return true
+				}
+			}
+
+			if SolveRecursive(data, clone, tx, ty) {
+				return true
+			} else {
+				data[x][y] = 0
+			}
+		}
+	}
+
+	y--
+	if y < 0 {
+		y = 8
+		x--
+	}
+
+	return false
+}
+
+func CreateClone(possibilities [][][]bool) [][][]bool {
+	ret := make([][][]bool, 9)
+	for x := range ret {
+		ret[x] = make([][]bool, 9)
+		for y := range ret[x] {
+			ret[x][y] = make([]bool, 9)
+			for v := range ret[x][y] {
+				ret[x][y][v] = possibilities[x][y][v]
+			}
+		}
+	}
+	return ret
+}
+
+func SetBlock(data [][]int, possibilities [][][]bool, x int, y int, val int) {
+	data[x][y] = val
+
+    // Rows
+    for ix := 0; ix < 9; ix++ {
+        possibilities[ix][y][val - 1] = false
+	}
+
+    // Columns
+    for iy := 0; iy < 9; iy++ {
+        possibilities[x][iy][val - 1] = false
+	}
+
+    // Cell
+    sx := x - (x % 3);
+    sy := y - (y % 3);
+    for ix := sx; ix < sx + 3; ix++ {
+        for iy := sy; iy < sy + 3; iy++ {
+			possibilities[ix][iy][val - 1] = false
+		}
+    }
+}
+
+func GeneratePossibilities(data [][]int) [][][]bool {
+	ret := make([][][]bool, 9)
+
+	for x := range ret {
+		ret[x] = make([][]bool, 9)
+		for y := range ret[x] {
+			ret[x][y] = make([]bool, 9)
+			for val := 1; val <= 9; val++ {
+				ret[x][y][val - 1] = 
+					data[x][y] == 0 &&
+					CheckRow(data, x, y, val) &&
+					CheckColumn(data, x, y, val) && 
+					CheckCell(data, x, y, val)
+			}
+		}
+	}
+	return ret
+}
+//END_CHALLENGE
+
+func CheckRow(data [][]int, x int, y int, val int) bool {
+	for ix := 0; ix < 9; ix++ {
+		if ix == x { 
+			continue 
+		}
+		if data[ix][y] == val { 
+			return false 
+		}
+	}
+	return true
+}
+
+func CheckColumn(data [][]int, x int, y int, val int) bool {
+	for iy := 0; iy < 9; iy++ {
+		if iy == y { 
+			continue 
+		}
+		if data[x][iy] == val {
+			return false 
+		}
+	}
+	return true
+}
+
+func CheckCell(data [][]int, x int, y int, val int) bool {
+	sx := x - (x % 3)
+	sy := y - (y % 3)
+
+	for ix := sx; ix < sx + 3; ix++ {
+		for iy := sy; iy < sy + 3; iy++ {
+			if y == iy && x == ix { 
+				continue 
+			}
+			if data[ix][iy] == val { 
+				return false 
+			}
+		}
+	}
+	return true
+}
---
+++ sudoku_test.go
@@ -0,0 +1,106 @@
+﻿package sudoku
+
+import "testing"
+
+func TestSolve1(t *testing.T) {
+	sudoku := ParseSudoku("000000000009805100051907420290401065000000000140508093026709580005103600000000000")
+	
+	if !IsValid(Solve(sudoku)) {
+		t.Error("Solution for Puzzle 1 is not valid")
+	}
+		t.Errorf("%", Solve(sudoku))
+}
+
+func TestSolve2(t *testing.T) {
+	sudoku := ParseSudoku("003020600900305001001806400008102900700000008006708200002609500800203009005010300")
+		
+	if !IsValid(Solve(sudoku)) {
+		t.Error("Solution for Puzzle 2 is not valid")
+	}
+}
+
+func TestSolve3(t *testing.T) {
+	sudoku := ParseSudoku("904200007010000000000706500000800090020904060040002000001607000000000030300005702")
+		
+	if !IsValid(Solve(sudoku)) {
+		t.Error("Solution for Puzzle 3 is not valid")
+	}
+}
+
+func ParseSudoku(sudoku string) [][]int {
+	data := make([][]int, 9)
+	for x := range data {
+		data[x] = make([]int, 9)
+		for y := range data[x] {
+			data[x][y] = int(sudoku[x * 9 + y] - 0x30)
+		}
+	}
+	return data
+}
+
+func IsValid(sudoku [][]int) bool {
+	// Create array and compute field sum
+	sum := 0
+	for x := range sudoku {
+		for y := range sudoku[x] {
+			sum += sudoku[x][y]
+		}
+	}
+
+	// Check field sum
+	if sum != 405 {
+		return false
+	}
+
+	// Check cells
+	for val := 1; val <= 9; val++ {
+		for cell := 0; cell < 9; cell++ {
+			count := 0
+			sx := cell % 3
+			sy := cell - sx
+			sx *= 3
+
+			for x := 0; x < 3; x++ {
+				for y := 0; y < 3; y++ {
+					if sudoku[x + sx][y + sy] == val {
+						count++
+						if count > 1 {
+							return false
+						}
+					}
+				}
+			}
+		}
+	}
+
+	// Check rows
+	for val := 1; val <= 9; val++ {
+		for y := 0; y < 9; y++ {
+			count := 0
+			for x := 0; x < 9; x++ {
+				if sudoku[x][y] == val {
+					count++
+					if count > 1 {
+						return false
+					}
+				}
+			}
+		}
+	}
+
+	// Check columns
+	for val := 1; val <= 9; val++ {
+		for x := 0; x < 9; x++ {
+			count := 0
+			for y := 0; y < 9; y++ {
+				if sudoku[x][y] == val {
+					count++
+					if count > 1 {
+						return false
+					}
+				}
+			}
+		}
+	}
+	return true
+}
