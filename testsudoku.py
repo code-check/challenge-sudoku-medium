798686c8a41fb89b7499ba7b3c5448ff495a8835
shunjikonishi <swandouble@gmail.com>
2016-03-24 15:18:10 +0900

    Mod test

--- testsudoku.py
+++ testsudoku.py
@@ -2,16 +2,22 @@ from nose.tools import assert_equal
 from sudoku import solve
 
 def test_solve_1():
-    solution = solve(parse_sudoku("000000000009805100051907420290401065000000000140508093026709580005103600000000000"))
-    assert_equal(True, is_valid(solution))
+    input = parse_sudoku("000000000009805100051907420290401065000000000140508093026709580005103600000000000")
+    output = solve(input)
+    assert_equal(True, is_valid(output))
+    assert_equal(True, compare(input, output))
 
 def test_solve_2():
-    solution = solve(parse_sudoku("003020600900305001001806400008102900700000008006708200002609500800203009005010300"))
-    assert_equal(True, is_valid(solution))
+    input = parse_sudoku("003020600900305001001806400008102900700000008006708200002609500800203009005010300")
+    output = solve(input)
+    assert_equal(True, is_valid(output))
+    assert_equal(True, compare(input, output))
 
 def test_solve_3():
-    solution = solve(parse_sudoku("904200007010000000000706500000800090020904060040002000001607000000000030300005702"))
-    assert_equal(True, is_valid(solution))
+    input = parse_sudoku("904200007010000000000706500000800090020904060040002000001607000000000030300005702")
+    output = solve(input)
+    assert_equal(True, is_valid(output))
+    assert_equal(True, compare(input, output))
 
 def parse_sudoku(data):
     chars = list(data)
@@ -65,3 +71,6 @@ def is_valid(data):
                     if count > 1:
                         return False
     return True
+
+def compare(input, output):
+    return all([all([input[y][x] == 0 or input[y][x] == output[y][x] for x in range(9)]) for y in range(9)])
